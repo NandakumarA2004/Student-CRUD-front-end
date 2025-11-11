88d8fe56da5c0487c33e16e8a923d75a47c50a71
Nandakumar <nandakumarpattoor2@gmail.com>
2025-11-11 13:30:46 +0530

    Change API base URL to production server

--- src/api/Config.jsx
+++ src/api/Config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://student-crud-back-end.onrender.com"
 });
 
 export const getStudents = () => {
